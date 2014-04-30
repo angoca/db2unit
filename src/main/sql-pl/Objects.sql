8b01e1cbaef542a7ee5c955725fd5813465113b7
Andres Gomez Casanova <angoca@yahoo.com>
2014-05-01 00:59:57 +0200

    New table for global reports

--- src/main/sql-pl/Objects.sql
+++ src/main/sql-pl/Objects.sql
@@ -15,15 +15,27 @@ Andres Gomez Casanova - initial API and implementation.
 SET CURRENT SCHEMA DB2UNIT_1A ;
 
 /**
- * Creates the objects that require high priviles.
+ * Creates the necessary objects to run the tests.
  *
  * Version: 2014-04-30 1-Alpha
  * Author: Andres Gomez Casanova (AngocA)
  * Made in COLOMBIA.
  */
 
--- Schema for logger tables.
-CREATE SCHEMA DB2UNIT_1A;
+-- Table for general reporting
+CREATE TABLE EXECUTION_REPORTS (
+  DATE TIMESTAMP NOT NULL,
+  EXECUTION_ID INT NOT NULL,
+  STATUS VARCHAR(16) NOT NULL,
+  MESSAGE VARCHAR(128) NOT NULL
+  );
 
-COMMENT ON SCHEMA DB2UNIT_1A IS 'Schema for db2unit objects';
+COMMENT ON TABLE EXECUTION_REPORTS IS 'Global report of the test executions';
+
+COMMENT ON EXECUTION_REPORTS (
+  DATE IS 'Date when the message of the test was generated',
+  EXECUTION_ID IS 'Unique ID of the execution',
+  STATUS IS 'Status of the test''s execution',
+  MESSAGE IS 'Descriptive message of the execution'
+  );
 
