3d4e85cd55a8800cb866a42519a4ca2b33020b3d
Andres Gomez Casanova <angoca@yahoo.com>
2014-05-03 12:08:34 +0200

    Report table as a model; column changed to diff from report

--- src/main/sql-pl/Objects.sql
+++ src/main/sql-pl/Objects.sql
@@ -22,12 +22,12 @@ SET CURRENT SCHEMA DB2UNIT_1A;
  * Made in COLOMBIA.
  */
 
--- Table for general reporting
+-- Table for general reporting.
 CREATE TABLE EXECUTION_REPORTS (
   DATE TIMESTAMP NOT NULL,
   EXECUTION_ID INT NOT NULL,
   STATUS VARCHAR(32) NOT NULL,
-  MESSAGE VARCHAR(128) NOT NULL
+  MESSAGE_REPORT VARCHAR(128) NOT NULL
   );
 
 COMMENT ON TABLE EXECUTION_REPORTS IS 'Global report of the test executions';
@@ -36,6 +36,23 @@ COMMENT ON EXECUTION_REPORTS (
   DATE IS 'Date when the message of the test was generated',
   EXECUTION_ID IS 'Unique ID of the execution',
   STATUS IS 'Status of the test''s execution',
-  MESSAGE IS 'Descriptive message of the execution'
+  MESSAGE_REPORT IS 'Descriptive message of the execution'
+  );
+
+-- Table for reports.
+CREATE TABLE REPORT_TESTS (
+  DATE TIMESTAMP NOT NULL,
+  EXECUTION_ID INT NOT NULL,
+  TEST_NAME VARCHAR(128) NOT NULL,
+  MESSAGE VARCHAR(512) NOT NULL
+  );
+
+COMMENT ON TABLE REPORT_TESTS IS 'Model table for reports';
+
+COMMENT ON REPORT_TESTS (
+  DATE IS 'Date when the message was generated',
+  EXECUTION_ID IS 'Unique ID of the execution',
+  TEST_NAME IS 'Name of test being executed',
+  MESSAGE IS 'Descriptive message about the currently executed test'
   );
 
