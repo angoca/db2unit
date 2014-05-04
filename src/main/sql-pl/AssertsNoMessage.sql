19fdb22ea2598c915334c5478884bd259a2e9c43
Andres Gomez Casanova <angoca@yahoo.com>
2014-05-04 13:45:09 +0200

    Assert true

---
+++ src/main/sql-pl/AssertsNoMessage.sql
@@ -0,0 +1,70 @@
+--#SET TERMINATOR @
+
+/*
+Copyright (c) 2014-2014 Andres Gomez Casanova (AngocA).
+
+All rights reserved. This program and the accompanying materials
+are made available under the terms of the Eclipse Public License v1.0
+which accompanies this distribution, and is available at
+http://www.eclipse.org/legal/epl-v10.html -->
+
+Contributors:
+Andres Gomez Casanova - initial API and implementation.
+*/
+
+SET CURRENT SCHEMA DB2UNIT_1A @
+
+/**
+ * Asserts implementation.
+ *
+ * Version: 2014-05-04 1-Alpha
+ * Author: Andres Gomez Casanova (AngocA)
+ * Made in COLOMBIA.
+ */
+
+/**
+ * Asserts if the given two strings are the same, in nullability, in length and
+ * in content.
+ *
+ * IN EXPECTED_STRING
+ *   Expected string.
+ * IN ACTUAL_STRING
+ *   Actual string.
+ */
+ALTER MODULE DB2UNIT ADD
+  PROCEDURE ASSERT_EQUALS (
+  IN EXPECTED_STRING VARCHAR(32672),
+  IN ACTUAL_STRING VARCHAR(32672)
+  )
+  LANGUAGE SQL
+  SPECIFIC P_ASSERT_EQUALS
+  DYNAMIC RESULT SETS 0
+  MODIFIES SQL DATA
+  NOT DETERMINISTIC
+  NO EXTERNAL ACTION
+  PARAMETER CCSID UNICODE
+ P_ASSERT_EQUALS: BEGIN
+  CALL ASSERT_EQUALS(NULL, EXPECTED_STRING, ACTUAL_STRING);
+ END P_ASSERT_EQUALS @
+
+/**
+ * Asserts if the given value is true.
+ *
+ * IN VALUE
+ *   Value to check against TRUE.
+ */
+ALTER MODULE DB2UNIT ADD
+  PROCEDURE ASSERT_TRUE (
+  IN VALUE BOOLEAN
+  )
+  LANGUAGE SQL
+  SPECIFIC P_ASSERT_TRUE
+  DYNAMIC RESULT SETS 0
+  MODIFIES SQL DATA
+  NOT DETERMINISTIC
+  NO EXTERNAL ACTION
+  PARAMETER CCSID UNICODE
+ P_ASSERT_TRUE: BEGIN
+  CALL ASSERT_TRUE(NULL, VALUE);
+ END P_ASSERT_TRUE @
+
