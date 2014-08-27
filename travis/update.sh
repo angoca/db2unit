f5bc20ef9a083d37fdc632cab0226f484b0898db
Andres Gomez Casanova <angoca@yahoo.com>
2014-08-27 21:18:48 +0200

    Prereqs

--- travis/update.sh
+++ travis/update.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-dpkg --add-architecture i386
 apt-get update -qq
-apt-get -y install libaio1 ksh libstdc++6-4.4-dev libstdc++6-4.4-pic libstdc++5 rpm
-#libpam0g:i386 numactl
-DEBIAN_FRONTEND=noninteractive apt-get install -qq libpam-ldap:i386
-#apt-get install libpam-modules:i386
-#apt-get install libacl1:i386
+apt-get install libaio1 lib32stdc++6 -y
+sudo apt-get install -qq libpam-ldap:i386
 
