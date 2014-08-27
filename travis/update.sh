5d51d3e7741f701663f95413bccf4375a9d15c69
Andres Gomez Casanova <angoca@yahoo.com>
2014-08-27 12:20:25 +0200

    Tests for 10.5 libs

--- travis/update.sh
+++ travis/update.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+dpkg --add-architecture i386
 apt-get update -qq
-apt-get -y install libaio1 ksh libstdc++6-4.4-pic libstdc++6-4.4-dev libstdc++5 rpm libpam0g:i386 numactl
+apt-get -y install libaio1 ksh libstdc++6-4.4-dev libstdc++6-4.4-pic libstdc++5 rpm
+#libpam0g:i386 numactl
+DEBIAN_FRONTEND=noninteractive apt-get install -qq libpam-ldap:i386
+#apt-get install libpam-modules:i386
+#apt-get install libacl1:i386
 
