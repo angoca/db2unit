66982f25e0251ba16364ad3668003edb43badba3
Andres Gomez Casanova <angoca@yahoo.com>
2014-10-07 00:06:09 +0200

    Test path variable used for scripts

--- src/test/scripts/init-dev.ps1
+++ src/test/scripts/init-dev.ps1
@@ -24,10 +24,12 @@
 # Made in COLOMBIA.
 
 # This variable indicates the location of the sources.
-${DB2UNIT_PATH}="..\..\.."
+if ( ! ( Test-Path Variable:\DB2UNIT_PATH ) ) {
+ ${DB2UNIT_PATH}="..\..\.."
 
-${Global:DB2UNIT_SRC_MAIN_CODE_PATH}="${DB2UNIT_PATH}\src\main\sql-pl"
-${Global:DB2UNIT_SRC_MAIN_SCRIPT_PATH}="${DB2UNIT_PATH}\src\main\scripts"
-${Global:DB2UNIT_SRC_TEST_CODE_PATH}="${DB2UNIT_PATH}\src\test\sql-pl"
-${Global:DB2UNIT_SRC_TEST_SCRIPT_PATH}="${DB2UNIT_PATH}\src\test\scripts"
+ ${Global:DB2UNIT_SRC_MAIN_CODE_PATH}="${DB2UNIT_PATH}\src\main\sql-pl"
+ ${Global:DB2UNIT_SRC_MAIN_SCRIPT_PATH}="${DB2UNIT_PATH}\src\main\scripts"
+ ${Global:DB2UNIT_SRC_TEST_CODE_PATH}="${DB2UNIT_PATH}\src\test\sql-pl"
+ ${Global:DB2UNIT_SRC_TEST_SCRIPT_PATH}="${DB2UNIT_PATH}\src\test\scripts"
+}
 
