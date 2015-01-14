84b23cd5d2130901f6fe3221b2cbf890c98641ac
Andres Gomez Casanova <angoca at yahoo or gmail>
2015-01-14 18:49:07 +0100

    Example for fails. Tests corrected

---
+++ src/examples/sql-pl/Tests_DB2UNIT_EXAMPLE_FAIL_ALL.sql
@@ -0,0 +1,64 @@
+--#SET TERMINATOR @
+
+/*
+ This file is part of db2unit: A unit testing framework for DB2 LUW.
+ Copyright (C)  2014  Andres Gomez Casanova (@AngocA)
+
+ db2unit is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ db2unit is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ Andres Gomez Casanova <angocaATyahooDOTcom>
+*/
+
+SET CURRENT SCHEMA DB2UNIT_EXAMPLE_FAIL_ALL @
+
+SET PATH = "SYSIBM","SYSFUN","SYSPROC","SYSIBMADM", DB2UNIT_2_BETA, DB2UNIT_EXAMPLE_FAIL_ALL @
+
+/**
+ * Set of test examples.
+ *
+ * Version: 2014-05-01 2_BETA
+ * Author: Andres Gomez Casanova (AngocA)
+ * Made in COLOMBIA.
+ */
+
+-- Before all tests.
+CREATE OR REPLACE PROCEDURE ONE_TIME_SETUP()
+ BEGIN
+  SIGNAL SQLSTATE 'TESTB' SET MESSAGE_TEXT = 'Fail in one time setup';
+ END @
+
+-- Before each test.
+CREATE OR REPLACE PROCEDURE SETUP()
+ BEGIN
+  -- Nothing.
+ END @
+
+-- After each test.
+CREATE OR REPLACE PROCEDURE TEAR_DOWN()
+ BEGIN
+  -- Nothing.
+ END @
+
+-- After all tests.
+CREATE OR REPLACE PROCEDURE ONE_TIME_TEAR_DOWN()
+ BEGIN
+  -- Nothing.
+ END @
+
+-- Test that passes correctly - Nothing.
+CREATE OR REPLACE PROCEDURE TEST_1()
+ BEGIN
+  -- Nothing.
+ END @
+
