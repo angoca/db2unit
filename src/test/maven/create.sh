1480a8493c5404fe8b91ea0b266f1d87b0fc43f7
Andres Gomez Casanova <angoca at yahoo or gmail>
2015-01-20 18:14:01 +0100

    URL

--- src/test/maven/create.sh
+++ src/test/maven/create.sh
@@ -23,7 +23,7 @@
 # Author: Andres Gomez Casanova (AngocA)
 # Made in COLOMBIA.
 set -xv
-TEMP_WIKI_DOC=/tmp/db2-link-server_t.md
+TEMP_WIKI_DOC=db2-link-server_t.md
 DB2_INSTALLER=v10.5fp5_linuxx64_server_t.tar.gz
 DB2_RSP_FILE_INSTALL=https://raw.githubusercontent.com/angoca/db2-docker/master/install/10.5/server_t/db2server_t.rsp
 DB2_RSP_FILE_INSTANCE=https://raw.githubusercontent.com/angoca/db2-docker/master/instance/server_t/db2server_t.rsp
