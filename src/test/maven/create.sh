53d40f314143c7cd1ee544058d09c1db77b4c78c
Andres Gomez Casanova <angoca at yahoo or gmail>
2015-01-20 23:18:01 +0100

    Libraries

--- src/test/maven/create.sh
+++ src/test/maven/create.sh
@@ -35,18 +35,12 @@ echo $DIR
 if [ ! -x ${DIR}/bin/db2 ] ; then
  echo "DB2 non installed"
 
- cat /etc/issue
- uname -a
  # Install libraries
- sudo dpkg --add-architecture i386
  sudo apt-get update
- sudo apt-get install -y \
-    aria2 \
-    curl \
-    libaio1 \
-    libpam-ldap:i386 \
-    libstdc++6-4.4-pic \
-    lib32stdc++6
+ sudo apt-get install libaio1 lib32stdc++6 -y
+ sudo apt-get install -qq libpam-ldap:i386
+ sudo ln -s /lib/i386-linux-gnu/libpam.so.0 /lib/libpam.so.0
+ sudo apt-get install -y aria2 curl
 
  wget https://raw.githubusercontent.com/wiki/angoca/db2-docker/db2-link-server_t.md
  URL=$(cat $(ls -1rt | tail -1) | tail -1)
