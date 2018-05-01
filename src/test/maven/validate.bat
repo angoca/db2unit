7f0bc2141a628d705a1e0531652c37d9ae8519b9
Andres Gomez Casanova <angoca at yahoo or gmail>
2018-05-01 10:56:09 -0500

    Validates if Db2 is correct in Windows

---
+++ src/test/maven/validate.bat
@@ -0,0 +1,29 @@
+@echo off
+::  This file is part of db2unit: A unit testing framework for DB2 LUW.
+:: Copyright (C)  2014  Andres Gomez Casanova (@AngocA)
+::
+:: db2unit is free software: you can redistribute it and/or modify
+:: it under the terms of the GNU General Public License as published by
+:: the Free Software Foundation, either version 3 of the License, or
+:: (at your option) any later version.
+::
+:: db2unit is distributed in the hope that it will be useful,
+:: but WITHOUT ANY WARRANTY; without even the implied warranty of
+:: MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+:: GNU General Public License for more details.
+::
+:: You should have received a copy of the GNU General Public License
+:: along with this program.  If not, see <http://www.gnu.org/licenses/>.
+::
+:: Andres Gomez Casanova <angocaATyahooDOTcom>
+
+:: Installs DB2, creates an instance and a database.
+::
+:: Version: 2015-01-14 V2_BETA
+:: Author: Andres Gomez Casanova (AngocA)
+:: Made in COLOMBIA.
+
+echo Db2 should be installed.
+echo Db2 should be configured.
+echo A Db2 instance should be running.
+
